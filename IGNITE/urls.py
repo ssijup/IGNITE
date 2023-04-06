98bc80b9a1625c84c0576584296b94f737cf918a
siju <ssijup@gmail.com>
2023-04-06 10:05:21 +0530

    one

---
+++ IGNITE/urls.py
@@ -0,0 +1,25 @@
+
+from django.contrib import admin
+from django.urls import path,include
+from django.views.generic import TemplateView
+from . import settings
+from django.conf.urls.static import static
+from products.views import view_404_error
+
+
+
+urlpatterns = [
+    path('index_3_home/',TemplateView.as_view(template_name='products/index_3_home.html'),name='index_3_home'),
+    path('admin/', admin.site.urls),
+    path('',include('products.urls')),
+   
+    path('accounts/', include('allauth.urls')),
+    
+
+            ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+#if settings.DEBUG:
+    #urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+
+handler404 = 'products.views.view_404_error'
